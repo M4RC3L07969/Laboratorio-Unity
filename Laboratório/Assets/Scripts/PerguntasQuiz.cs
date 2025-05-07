9dfa2d13d4427c7a627bd448f103d0e7e50df0c3
M4RC3L07969 <jrmaia.marcelo@gmail.com>
2025-05-07 17:01:48 -0300

    Quiz pt4

--- Laboratório/Assets/Scripts/PerguntasQuiz.cs
+++ Laboratório/Assets/Scripts/PerguntasQuiz.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using Random = System.Random;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.SceneManagement;
 
 public class PerguntasQuiz : MonoBehaviour
 {
@@ -16,64 +15,57 @@ public class PerguntasQuiz : MonoBehaviour
         public Button button2;
         public Button button3;
         public Button button4;
-        public Text acertos;
-
-    public Color correctColor = Color.green; 
-    public Color wrongColor = Color.red;     
-    public Color normalColor = Color.white;  
-
 
     [Header("Player's Quiz Life")]
 
-    public int lifeQuiz = 3;
+        public int lifeQuiz = 5;
 
-    private String[] listaDePerguntas = new String[45] {"O que são átomos e moléculas?",
-            "Qual a diferença entre substâncias puras e misturas?",
-            "O que caracteriza uma mudança física e uma mudança química?",
-            "Quais são os estados físicos da matéria?",
-            "O que é a tabela periódica e como ela está organizada?",
-            "O que são metais, ametais e gases nobres?",
-            "Qual a diferença entre substâncias simples e compostas?",
-            "O que são reações químicas? Dê um exemplo.",
-            "O que é um elemento químico?",
-            "O que são ácidos e bases?",
-            "O que são ligações iônicas e covalentes?",
-            "Como ocorre a ligação metálica?",
-            "O que é eletronegatividade e como ela influencia as ligações químicas?",
-            "Qual a diferença entre reações endotérmicas e exotérmicas?",
-            "O que são coeficientes estequiométricos?",
-            "O que é um mol e qual sua relação com a constante de Avogadro?",
-            "Como se classificam as funções inorgânicas (ácidos, bases, sais e óxidos)?",
-            "O que são soluções e quais são suas classificações?",
-            "Como calcular a concentração de uma solução?",
-            "O que é a lei da conservação da massa de Lavoisier?",
-            "Como balancear uma equação química?",
-            "Qual a importância do pH e como ele é calculado?",
-            "O que são reações de oxidação e redução?",
-            "O que é cinética química e quais fatores influenciam a velocidade de uma reação?",
-            "O que é catalisador e qual seu papel nas reações químicas?",
-            "Como funcionam os equilíbrios químicos?",
-            "O que é a constante de equilíbrio (Kc)?",
-            "Como a pressão e a temperatura afetam um equilíbrio químico?",
-            "O que diz o princípio de Le Chatelier?",
-            "O que são reações reversíveis e irreversíveis?",
-            "O que é solubilidade e quais fatores a influenciam?",
-            "O que é eletroquímica e como funcionam as pilhas eletroquímicas?",
-            "Qual a diferença entre eletrólise ígnea e eletrólise aquosa?",
-            "O que é química orgânica e qual sua importância?",
-            "O que são hidrocarbonetos e como são classificados?",
-            "O que são grupos funcionais na química orgânica?",
-            "O que é isomeria e quais são seus tipos?",
-            "Como funcionam as reações de substituição, adição e eliminação?",
-            "O que é polimerização e quais são os principais tipos de polímeros?",
-            "O que são biomoléculas e qual sua importância para a vida?",
-            "O que é a radioatividade e como ela foi descoberta?",
-            "Como funcionam as reações nucleares de fissão e fusão?",
-            "O que são os gases do efeito estufa e qual seu impacto ambiental?",
-            "Como os combustíveis fósseis afetam o meio ambiente?",
-            "O que são energias renováveis e como a química contribui para seu desenvolvimento?"};
+        private String[] listaDePerguntas = new String[45] {"O que são átomos e moléculas?",
+                "Qual a diferença entre substâncias puras e misturas?",
+                "O que caracteriza uma mudança física e uma mudança química?",
+                "Quais são os estados físicos da matéria?",
+                "O que é a tabela periódica e como ela está organizada?",
+                "O que são metais, ametais e gases nobres?",
+                "Qual a diferença entre substâncias simples e compostas?",
+                "O que são reações químicas? Dê um exemplo.",
+                "O que é um elemento químico?",
+                "O que são ácidos e bases?",
+                "O que são ligações iônicas e covalentes?",
+                "Como ocorre a ligação metálica?",
+                "O que é eletronegatividade e como ela influencia as ligações químicas?",
+                "Qual a diferença entre reações endotérmicas e exotérmicas?",
+                "O que são coeficientes estequiométricos?",
+                "O que é um mol e qual sua relação com a constante de Avogadro?",
+                "Como se classificam as funções inorgânicas (ácidos, bases, sais e óxidos)?",
+                "O que são soluções e quais são suas classificações?",
+                "Como calcular a concentração de uma solução?",
+                "O que é a lei da conservação da massa de Lavoisier?",
+                "Como balancear uma equação química?",
+                "Qual a importância do pH e como ele é calculado?",
+                "O que são reações de oxidação e redução?",
+                "O que é cinética química e quais fatores influenciam a velocidade de uma reação?",
+                "O que é catalisador e qual seu papel nas reações químicas?",
+                "Como funcionam os equilíbrios químicos?",
+                "O que é a constante de equilíbrio (Kc)?",
+                "Como a pressão e a temperatura afetam um equilíbrio químico?",
+                "O que diz o princípio de Le Chatelier?",
+                "O que são reações reversíveis e irreversíveis?",
+                "O que é solubilidade e quais fatores a influenciam?",
+                "O que é eletroquímica e como funcionam as pilhas eletroquímicas?",
+                "Qual a diferença entre eletrólise ígnea e eletrólise aquosa?",
+                "O que é química orgânica e qual sua importância?",
+                "O que são hidrocarbonetos e como são classificados?",
+                "O que são grupos funcionais na química orgânica?",
+                "O que é isomeria e quais são seus tipos?",
+                "Como funcionam as reações de substituição, adição e eliminação?",
+                "O que é polimerização e quais são os principais tipos de polímeros?",
+                "O que são biomoléculas e qual sua importância para a vida?",
+                "O que é a radioatividade e como ela foi descoberta?",
+                "Como funcionam as reações nucleares de fissão e fusão?",
+                "O que são os gases do efeito estufa e qual seu impacto ambiental?",
+                "Como os combustíveis fósseis afetam o meio ambiente?",
+                "O que são energias renováveis e como a química contribui para seu desenvolvimento?"};
 
-        
         string[][] listaDeRespostas = new string[][]
     {
         new string[]
@@ -394,13 +386,13 @@ public class PerguntasQuiz : MonoBehaviour
     };
 
     private Random random;
+
     private int indicePerguntaAtual;
-    public string respostaSelecionada;
+    private string respostaCorreta;
+    private HashSet<int> indicesRespondidos = new HashSet<int>();
+
     private int vezesMostrada = 0;
-    private int vezesAcertada = 0;
-    private const int MinAcertos= 4;
-
-
+    private const int maximoRepeticoes = 5;
 
     void Start()
     {
@@ -409,15 +401,15 @@ public class PerguntasQuiz : MonoBehaviour
 
     void CarregarPergunta()
     {
-        if (vezesAcertada >= MinAcertos)
+        if (vezesMostrada >= maximoRepeticoes)
         {
-            SceneManager.LoadScene("Cenário");
+            Debug.Log("Fim das repetições da pergunta.");
+            return;
         }
 
-        if (lifeQuiz <= 0)
+        if(lifeQuiz <= 0)
         {
-            SceneManager.LoadScene("TelaInicial");
-            return;
+            Debug.Log("Você perdeu.");
         }
 
         random = new Random();
@@ -426,104 +418,36 @@ public class PerguntasQuiz : MonoBehaviour
         titulo.text = listaDePerguntas[indicePerguntaAtual];
 
         string[] respostas = (string[])listaDeRespostas[indicePerguntaAtual].Clone();
+        respostaCorreta = listaDeRespostas[indicePerguntaAtual][0];
+
         for (int i = respostas.Length - 1; i > 0; i--)
         {
             int j = random.Next(i + 1);
             string temp = respostas[i];
             respostas[i] = respostas[j];
-            respostas[j] = temp;
+            respostas[j] = temp; 
         }
 
         button1.GetComponentInChildren<TMP_Text>().text = respostas[0];
         button2.GetComponentInChildren<TMP_Text>().text = respostas[1];
         button3.GetComponentInChildren<TMP_Text>().text = respostas[2];
         button4.GetComponentInChildren<TMP_Text>().text = respostas[3];
-
-        if (vezesMostrada != 0)
-        {
-            button1.onClick.RemoveAllListeners();
-            button2.onClick.RemoveAllListeners();
-            button3.onClick.RemoveAllListeners();
-            button4.onClick.RemoveAllListeners();
-        }
-
-        button1.onClick.AddListener(() => VerificarResposta(respostas[0],button1));
-        button2.onClick.AddListener(() => VerificarResposta(respostas[1],button2));
-        button3.onClick.AddListener(() => VerificarResposta(respostas[2],button3));
-        button4.onClick.AddListener(() => VerificarResposta(respostas[3],button4));
-        
-    }
-    private void Update()
-    {
-        acertos.text = "" + vezesAcertada + "/4";
     }
 
-    public void VerificarResposta(string respostaSelecionada, Button clickedButton)
+    public void VerificarResposta(string respostaSelecionada)
     {
-        if (respostaSelecionada == listaDeRespostas[indicePerguntaAtual][0])
+        if (string.Equals(respostaSelecionada, listaDeRespostas[indicePerguntaAtual][0]))
         {
             Debug.Log("Acertou!");
-            vezesAcertada++;
             vezesMostrada++;
-            SetButtonColor(clickedButton, correctColor);
-            CarregarPergunta();
+            CarregarPergunta(); 
         }
         else
-        {      
+        {
             Debug.Log("Errou!");
             lifeQuiz--;
-            vezesMostrada++;
-            SetButtonColor(clickedButton, wrongColor);
-            SetButtonColor(FindCorrectButton(), correctColor);
             CarregarPergunta();
+
         }
-        StartCoroutine(EsperarEResetarCores(2f));
-    }
-    private Button FindCorrectButton()
-    {
-
-        string respostaCorreta = listaDeRespostas[indicePerguntaAtual][0];
-
-
-        if (button1.GetComponentInChildren<TMP_Text>().text == respostaCorreta)
-        {
-            return button1;
-        }
-        else if (button2.GetComponentInChildren<TMP_Text>().text == respostaCorreta)
-        {
-            return button2;
-        }
-        else if (button3.GetComponentInChildren<TMP_Text>().text == respostaCorreta)
-        {
-            return button3;
-        }
-        else
-        {
-            return button4;
-        }
-    }
-
-    private IEnumerator EsperarEResetarCores(float tempo)
-    {
-        yield return new WaitForSeconds(tempo);
-        ResetButtonColors();
-        CarregarPergunta(); 
-    }
-    private void SetButtonColor(Button button, Color color)
-    {
-        button.GetComponent<Image>().color = color;
-    }
-
-    private void ResetButtonColors()
-    {
-        SetButtonNormal(button1);
-        SetButtonNormal(button2);
-        SetButtonNormal(button3);
-        SetButtonNormal(button4);
-    }
-
-    private void SetButtonNormal(Button button)
-    {
-        button.GetComponent<Image>().color = normalColor;
     }
 }
