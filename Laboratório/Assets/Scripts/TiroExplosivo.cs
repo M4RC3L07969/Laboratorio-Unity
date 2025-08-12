fa2beca85e65c12ff156e07932af7454b23c27b2
lucasvrcc <lucasvalentim@soulasalle.com.br>
2025-08-12 18:10:15 -0300

    PU Explosivo (ainda não pronto)

--- Laboratório/Assets/Scripts/TiroExplosivo.cs
+++ Laboratório/Assets/Scripts/TiroExplosivo.cs
@@ -4,9 +4,25 @@ public class TiroExplosivo : MonoBehaviour
 {
     public Vector3 rotationAxis = Vector3.up;
     public float rotationSpeed = 60f;
+    public bool tiroExplosivo;
 
     void Update()
     {
         transform.Rotate(rotationAxis * rotationSpeed * Time.deltaTime);
+
+        if (tiroExplosivo == true)
+        {
+            Debug.Log("Ativo Tiro Explosivo");
+        }
     }
-}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            tiroExplosivo = true;
+            Destroy(gameObject);
+        }
+
+        }
+    }
