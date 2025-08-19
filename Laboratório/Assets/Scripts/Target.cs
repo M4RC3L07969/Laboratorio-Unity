12f12011585fd0e3101cb4cce3cfe1ed48c15996
Pedro Augusto <pedrooknd@gmail.com>
2025-08-19 17:35:19 -0300

    inimigo atualizado

--- Laboratório/Assets/Scripts/Target.cs
+++ Laboratório/Assets/Scripts/Target.cs
@@ -3,38 +3,52 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 30f;
-    public float velocidade = 2f;
+    public float velocidade = 10f;
     public Rigidbody inimigoRb;
     public GameObject player;
     private Animator animator;
+    private PlayerMovement playerMovement;
+    private string[] action = { "kick", "punch" };
 
     void Start()
     {
         inimigoRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+        if (player != null)
+            playerMovement = player.GetComponent<PlayerMovement>();
         animator = GetComponent<Animator>();
+   
     }
 
     void Update()
     {
-        Vector3 direction = (player.transform.position).normalized;
+        if (player == null) return;
+        float distance = Vector3.Distance(transform.position, player.transform.position);
 
-        inimigoRb.AddForce(direction * velocidade);
+        
+        if (distance < 4f)
+        {
+            string actualAction = action[UnityEngine.Random.Range(0, action.Length)];
+            animator.SetTrigger(actualAction);
+            playerMovement.TakeDamage(1);
+        }
 
+        else
+        {
+            
+            animator.SetBool("isNear", true);
+
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            
+            Debug.Log($"Direção: {direction}, Posição Player: {player.transform.position}, Posição Inimigo: {transform.position}");
+            
+            transform.position += direction * velocidade * Time.deltaTime;
+        }
+
+   
         Vector3 lookAtPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
         transform.LookAt(lookAtPosition);
-        animator.SetBool("isNear",true);
-       
-
     }
 
-    public void TakeDamage(float amount)
-    {
-        animator.SetTrigger("hit");
-        health -= amount;
-        if (health <= 0f)
-        {
-            Destroy(gameObject);
-        }
-    }
+
 }
